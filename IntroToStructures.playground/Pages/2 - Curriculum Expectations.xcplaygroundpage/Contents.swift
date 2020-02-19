218c08bfec2f36ee5fa3bcab28fdd9e1dcab12eb
Russell Gordon <github@russellgordon.ca>
2020-02-19 10:32:29 -0500

    Got two initial pages done (introduction and curriculum expectations).

--- IntroToStructures.playground/Pages/2 - Curriculum Expectations.xcplaygroundpage/Contents.swift
+++ IntroToStructures.playground/Pages/2 - Curriculum Expectations.xcplaygroundpage/Contents.swift
@@ -7,25 +7,34 @@
  
  **A1.1:** Use constants and variables, including integers, floating points, strings, and Boolean values, correctly in computer programs.
  
- **A1.4:** demonstrate the ability to use... arithmetic operators (e.g., addition, subtraction, multiplication, division, exponentiation, parentheses), and order of operations correctly in computer programs;
+ **A1.3:** Use assignment statements correctly with both arithmetic and string expressions in computer
+          programs;
  
- **A3.2:** write subprograms (e.g., functions, procedures) that use parameter passing and appropriate variable scope (e.g., local, global), to perform tasks within programs.
+ **A1.4:** Demonstrate the ability to use Boolean operators (e.g., AND, OR, NOT), comparison operators (i.e., equal to, not equal to, greater than, less than, greater than or equal to, less than or equal to), arithmetic operators (e.g., addition, subtraction, multiplication, division, exponentiation, parentheses), and order of operations correctly in computer programs;;
+ 
+ **A2.2:** Use sequence [and] selection control structures to create programming solutions.
+ 
+ **A2.3**: Write algorithms with nested structures (e.g., to count elements in an array, calculate a total, find
+ highest or lowest value, or perform a linear search).
+ 
+ **A3.2:** Write subprograms (e.g., functions, procedures) that use parameter passing and appropriate variable scope (e.g., local, global), to perform tasks within programs.
 
  ## Communication
  
  **A4.2:** Use workplace and professional conventions (e.g., naming, indenting, commenting) correctly to write programs and internal documentation;
- 
- **A4.5:** Demonstrate the ability to validate a program using a full range of test cases.
- 
+  
  ## Thinking
  
  **B2.3:** Apply the principle of modularity to design reusable code (e.g., subprograms, classes) in computer programs;
  
  **B3.2:** solve common problems (e.g., calculation of hypotenuse, determination of primes, calculation of area and circumference) by applying mathematical equations or formulas in an algorithm;
  
+ **B3.3:** design algorithms to detect, intercept, and handle exceptions (e.g., division by zero, roots of
+ negatives).
+ 
  Be sure to document what you complete in your portfolio for this thread.
  
- [Previous: Introduction](@previous) | Page 2 | [Next: About Abstractions](@next)
+ [Previous: Introduction](@previous) | Page 2 | [Next: ???](@next)
 
 */
 
